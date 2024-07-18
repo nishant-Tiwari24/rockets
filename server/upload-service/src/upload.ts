6c33e86c41cec05f30db06f98c103ba5fac5fca1
its-nishant320 <124623023+its-nishant320@users.noreply.github.com>
2024-07-18 11:34:31 +0530

    env variables

--- server/upload-service/src/upload.ts
+++ server/upload-service/src/upload.ts
@@ -2,10 +2,9 @@ import { S3 } from "aws-sdk";
 import fs from "fs";
 
 const s3 = new S3({
-  accessKeyId: "7ea9c3f8c7f0f26f0d21c5ce99d1ad6a",
-  secretAccessKey:
-    "b4df203781dd711223ce931a2d7ca269cdbf81bb530de4548474584951b798be",
-  endpoint: "https://e21220f4758c0870ba9c388712d42ef2.r2.cloudflarestorage.com",
+  accessKeyId: process.env.ACCESSID,
+  secretAccessKey: process.env.ACCESSKEY,
+  endpoint: process.env.ENDPOINT,
 });
 
 export function upload(file: string, fileName: string) {
