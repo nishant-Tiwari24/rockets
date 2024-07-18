9abc69a823f2ecdac1c45cefe657a74da428e451
its-nishant320 <124623023+its-nishant320@users.noreply.github.com>
2024-07-18 11:30:40 +0530

    client

--- server/deploy-service/src/index.ts
+++ server/deploy-service/src/index.ts
@@ -1,10 +1,13 @@
 import { createClient, commandOptions } from "redis";
-import { downloadS3Folder } from "./aws";
+import { copyFinalDist, downloadS3Folder } from "./aws";
 import express from "express";
 import { buildProject } from "./execute";
 
 const subscriber = createClient();
 subscriber.connect();
+
+const publisher = createClient();
+publisher.connect();
 
 async function sub() {
   while (true) {
@@ -19,6 +22,8 @@ async function sub() {
         const id = response.element[1];
         await downloadS3Folder(`output${id}`);
         await buildProject(id);
+        copyFinalDist(id);
+        publisher.hSet("status", id, "deployed");
       }
     } catch (error) {
       console.error("Error processing message from Redis:", error);
--- server/upload-service/src/index.ts
+++ server/upload-service/src/index.ts
@@ -11,6 +11,9 @@ app.use(express.json());
 const publisher = createClient();
 publisher.connect();
 
+const subscriber = createClient();
+publisher.connect();
+
 app.post("/send-url", async (req, res) => {
   const repoUrl = req.body.repoUrl;
   const id = random();
@@ -21,8 +24,18 @@ app.post("/send-url", async (req, res) => {
   files.forEach(async (element) => {
     await upload(element, element.slice(__dirname.length + 1));
   });
+  await new Promise((resolve) => setTimeout(resolve, 5000));
   publisher.lPush("build-queue", id);
+  publisher.hSet("status", id, "uploaded");
   res.json({ generated: id, files });
+});
+
+app.get("/status", async (req, res) => {
+  const id = req.query.id;
+  const response = await subscriber.hGet("status", id as string);
+  res.json({
+    status: response,
+  });
 });
 
 app.listen(5500, () => {
